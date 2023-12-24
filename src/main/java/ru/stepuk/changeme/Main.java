c503cdb4acf3f7a58f4d40c016d80d9b270c5895
Ivan Stepuk <ivan.stepuk@outlook.com>
2023-12-24 16:08:55 +0300

    Spaces

--- src/main/java/ru/stepuk/changeme/Main.java
+++ src/main/java/ru/stepuk/changeme/Main.java
@@ -2,11 +2,11 @@ package ru.stepuk.changeme;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println(Runtime.version());
-	}
+    public static void main(String[] args) {
+        System.out.println(Runtime.version());
+    }
 
-	public int add(int x, int y) {
-		return x + y;
-	}
+    public int add(int x, int y) {
+        return x + y;
+    }
 }
